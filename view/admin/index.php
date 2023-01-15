a3edd97c4b0c95f11c473b2f5d491199bd65e7f1
Audric Rosier <audricrosier@gmail.com>
2023-01-15 23:58:31 +0100

    Update index.php

--- view/admin/index.php
+++ view/admin/index.php
@@ -1,6 +1,20 @@
 <?php
-session_start();
-var_dump($_SESSION);
+
+if (!isset($_SESSION['logged'])) {
+    header("location: /admin/login");
+    die;
+    
+} else {
+    $adminDAO = new AdminDAO;
+    $adminConnected = $adminDAO->fetch($_SESSION['logged']);
+
+    if (!$adminConnected) {
+        session_unset();
+        header("location: /");
+        die;
+    }
+}
+
 ?>
 
 <nav class="admin">
