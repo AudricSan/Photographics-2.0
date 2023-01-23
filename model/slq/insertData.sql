6bbe6490df462cc0a1c1ccd5a195f187e09b0341
Audric Rosier <audricrosier@gmail.com>
2023-01-23 18:00:31 +0100

    Create insertData.sql

---
+++ model/slq/insertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO `role`  (`role_id`, `role_name`) VALUES (NULL, 'Administrator'),
+                                                    (NULL, 'Editor'),
+                                                    (NULL, 'Photographer');
