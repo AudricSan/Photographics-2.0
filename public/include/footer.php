e7a59ea4285290824df594989190e310e5fa7dad
Audric Rosier <audricrosier@gmail.com>
2023-01-24 22:20:16 +0100

    Create footer.php

---
+++ public/include/footer.php
@@ -0,0 +1,50 @@
+<footer>
+    <div class='footer'>
+        <div class='footerleft'>
+            <a href=''> <img src='../images/logo.png' alt='Logo'> </a> </figure>
+            <h3> <a href=''> Photographer Name </a> </h3>
+            <p> Photographics is a free and open source project for photographers who want to swiftly submit their
+                photos to the internet.
+                This project seeks to make managing your photographs as simple and quick as possible. </br>
+                It gives the photographer total control over his images by allowing him to tag and share them.
+                </br></br> Photographics is an open source project that is completely free!
+                </br> A site where you may save all of your images in one location. </p>
+        </div>
+
+        <div class='footerright'>
+            <div>
+                <div class="footermenu">
+                    <h3> Photographics </h3>
+                    <ul>
+                        <li> <a href=''>Gallery</a> </li>
+                        <li> <a href=''>Gallery</a> </li>
+                        <li> <a href=''>Gallery</a> </li>
+                        <li> <a href=''>Gallery</a> </li>
+                        <li> <a href=''>Gallery</a> </li>
+                    </ul>
+                </div>
+
+                <div class="footermenu">
+                    <h3> Photographer Name </h3>
+                    <ul>
+                        <li><a href=''>About</a></li>
+                        <li><a href=''>Contact</a></li>
+                        <li><a href=''> Facebook </a></li>
+                        <li><a href=''> Instagram </a></li>
+                        <li><a href=''> Twitch </a></li>
+                        <li><a href=''> Twitter </a></li>
+                        <li><a href=''> Linkedin </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class='copyright'>
+                <a href=''> &copy;Photographics Project - 2021</a>
+                <a>|</a>
+                <a href='/privacy'>Privacy-Terms</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="../js/error.js"></script>
