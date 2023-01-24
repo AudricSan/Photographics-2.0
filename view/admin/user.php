b57bc4ecc52e3a2fa594d6ab056df7b1842b44fe
Audric Rosier <audricrosier@gmail.com>
2023-01-24 22:19:43 +0100

    Create user.php

---
+++ view/admin/user.php
@@ -0,0 +1,46 @@
+<?php
+
+if (!isset($_SESSION['logged'])) {
+    header("location: /admin/login");
+    die;
+} else {
+    $adminDAO = new AdminDAO;
+    $adminConnected = $adminDAO->fetch($_SESSION['logged']);
+
+    if (!$adminConnected) {
+        session_unset();
+        header("location: /");
+        die;
+    }
+}
+
+// $imgroot = $_SESSION['imgroot'];
+
+?>
+<main class="admin">
+        <h1> <a href='/admin/newuser' > <i class="fa-solid fa-user-plus"></i> </a> </h1>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Mail</th>
+                    <th>Role</th>
+                    <th>Quick Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td class=action>
+                        <a class='btn edit' href=''>Edit</a>
+                        <a class='btn delete' href=''>Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</main>
