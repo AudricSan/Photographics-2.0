a310efca1cbb21eba28518e80d758548aff02ef6
Audric Rosier <audricrosier@gmail.com>
2023-01-27 14:02:41 +0100

    Update user.php

--- view/admin/user.php
+++ view/admin/user.php
@@ -23,17 +23,18 @@ if (!isset($_SESSION['logged'])) {
     </h1>
 
     <?php
-        // var_dump($_SESSION);
+    // var_dump($_SESSION);
 
-        if (isset($_SESSION['error']) && !empty($_SESSION['error'])) {
-            $error = $_SESSION['error'];
+    if (isset($_SESSION['error']) && !empty($_SESSION['error'])) {
+        foreach ($_SESSION['error'] as $value) {
             echo "
-                <div class='error'>
-                    <p> Something Wrong : <span> $error </span></p>
-                </div>
-            ";
-            unset($_SESSION['error']);
+            <div class='error'>
+                <p> Something Wrong : <span> $value </span></p>
+            </div>
+        ";
         }
+        unset($_SESSION['error']);
+    }
     ?>
 
     <table>
@@ -45,31 +46,29 @@ if (!isset($_SESSION['logged'])) {
                 <th>Quick Action</th>
             </tr>
         </thead>
+        <?php
+        $adminDAO = new AdminDAO;
+        $roleDAO = new RoleDAO;
+        $users = $adminDAO->fetchAll();
 
-        <tbody>
-            <tr>
-                <?php
-                $adminDAO = new AdminDAO;
-                $roleDAO = new RoleDAO;
-                $users = $adminDAO->fetchAll();
+        foreach ($users as $user) {
+            $role = $roleDAO->fetch($user->_role);
 
-                foreach ($users as $user) {
-                    $role = $roleDAO->fetch($user->_role);
+            echo "
+                <tbody>
+                    <tr>
+                        <td> $user->_name </td>
+                        <td> $user->_mail </td>
+                        <td> $role->_name </td>
 
-                    echo "
-                            <td> $user->_name </td>
-                            <td> $user->_mail </td>
-                            <td> $role->_name </td>
-
-                            <td class=action>
-                                <a class='btn edit'   href='/admin/newuser?$user->_id'>Edit</a>
-                                <a class='btn delete' href='/admin/deluser?$user->_id'>Delete</a>
-                            </td>
-                        ";
-                }
-                ?>
-            </tr>
-        </tbody>
+                        <td class=action>
+                            <a class='btn edit'   href='/admin/newuser?user=$user->_id'>Edit</a>
+                            <a class='btn delete' href='/admin/deluser?user=$user->_id'>Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            ";
+        }
+        ?>
     </table>
-    </div>
 </main>
